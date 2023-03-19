004060acf67f65fcf2a40762ae179b70155413a6
Nikolay Dutskinov <niki.dutskinov@gmail.com>
2023-03-19 15:57:31 +0200

    Initial commit

---
+++ Countries/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Countries
+//
+//  Created by Nikolay N. Dutskinov on 19.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# Countries
+ Sample application that lists many countries from all over the world. The application is planned to use MVVM & Input/Output
