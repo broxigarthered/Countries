23b9cc67f7fc86893f7f8224b528b1666946ad33
Nikolay Dutskinov <niki.dutskinov@gmail.com>
2023-03-21 12:48:33 +0200

    Removed unused code from NetworkManager

--- Countries/API/NetworkManager.swift
+++ Countries/API/NetworkManager.swift
@@ -7,29 +7,27 @@
 
 import Foundation
 
-
-
-// TODO: Stay to this file but do some refactoring
-
 // MARK: - Data task protocol
 protocol URLSessionDataTaskProtocol {
+    
     func resume()
+    
 }
 
 protocol URLSessionProtocol {
+    
     typealias DataTaskResult = (Data?, URLResponse?, Error?) -> Void
     
     func dataTask(with request: URLRequest, completionHandler: @escaping DataTaskResult) -> URLSessionDataTaskProtocol
+    
 }
 
 protocol HttpRequestable {
-//    typealias Completion = (_ data: Data?, _ error: NetworkError?) -> Void
-//    func request(endpoint: Endpoint, completion: @escaping Completion)
     
-    
-    // TODO: choose one of both
     typealias SecondCompletion = (_ result: Result<Data, NetworkError>) -> Void
+    
     func request(endpoint: Endpoint, completion: @escaping SecondCompletion)
+    
 }
 
 class NetworkManager: HttpRequestable {
@@ -59,34 +57,6 @@ class NetworkManager: HttpRequestable {
         
         task.resume()
     }
-//
-//    func request(endpoint: Endpoint, completion: @escaping (Data?, NetworkError?) -> Void) {
-//        guard let url = URL(string: endpoint.baseUrl + endpoint.endpoint) else {
-//            completion(nil, .invalidURL)
-//            return
-//        }
-//        var request = URLRequest(url: url)
-//        request.httpMethod = endpoint.method.rawValue
-//
-//        let task = session.dataTask(with: request) { (data, response, error) in
-//            guard let response = response as? HTTPURLResponse else {
-//                return completion(nil, NetworkError.invalidResponse)
-//
-//            }
-//
-//            guard 200..<300 ~= response.statusCode else {
-//                completion(nil, NetworkError.error(statusCode: response.statusCode, data: data))
-//                return
-//            }
-//
-//            completion(data, nil)
-//        }
-//        task.resume()
-//    }
-    
-    
-//    typealias completeClosure = ( _ data: Data?, _ error: Error?)->Void
-//    typealias Completion = (_ data: Data?, _ error: NetworkError?) -> Void
 
     private let session: URLSessionProtocol
     
