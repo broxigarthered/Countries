18ee9ee888a5089eb1d21cf463e0a052d961467c
Nikolay Dutskinov <niki.dutskinov@gmail.com>
2023-03-23 18:20:17 +0200

    Finished the implementation of the design for CountryDetailsViewController

--- Countries/Flows/Countries/CountryDetails/View/CountryDetailsViewController.swift
+++ Countries/Flows/Countries/CountryDetails/View/CountryDetailsViewController.swift
@@ -7,8 +7,12 @@
 
 import UIKit
 import SVGKit
+import MapKit
 
 class CountryDetailsViewController: UIViewController, SkeletonLoadable {
+    
+    private let viewModel: CountryDetailsViewModel
+    private let cache: ImageCacheType
     
     @IBOutlet weak var flagImageView: UIImageView! {
         didSet {
@@ -26,8 +30,52 @@ class CountryDetailsViewController: UIViewController, SkeletonLoadable {
         }
     }
     
-    private let viewModel: CountryDetailsViewModel
-    private let cache: ImageCacheType
+    @IBOutlet weak var countryNameLabel: UILabel! {
+        didSet {
+            countryNameLabel.numberOfLines = 0
+            countryNameLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
+            countryNameLabel.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+            countryNameLabel.text = viewModel.country.name
+        }
+    }
+    
+    @IBOutlet weak var regionNameLabel: UILabel! {
+        didSet {
+            regionNameLabel.numberOfLines = 0
+            regionNameLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
+            regionNameLabel.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+            regionNameLabel.text = viewModel.country.region
+        }
+    }
+    
+    @IBOutlet weak var populationLabel: UILabel! {
+        didSet {
+            populationLabel.numberOfLines = 0
+            populationLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
+            populationLabel.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+            populationLabel.text = String(viewModel.country.population)
+        }
+    }
+    
+    @IBOutlet weak var capitalNameLabel: UILabel! {
+        didSet {
+            capitalNameLabel.numberOfLines = 0
+            capitalNameLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
+            capitalNameLabel.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+            capitalNameLabel.text = viewModel.country.capitalName
+        }
+    }
+    
+    @IBOutlet weak var mapKitView: MKMapView! {
+        didSet {
+            let latitude: CLLocationDegrees = viewModel.country.latLng[0]
+            let longitude: CLLocationDegrees = viewModel.country.latLng[1]
+            let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+            let span = MKCoordinateSpan(latitudeDelta: 8, longitudeDelta: 8)
+            let region = MKCoordinateRegion(center: location, span: span)
+            mapKitView.setRegion(region, animated: true)
+        }
+    }
     
     init(viewModel: CountryDetailsViewModel, imageCache: ImageCacheType) {
         self.viewModel = viewModel
